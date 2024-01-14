ae102915ff98fe17d88b147893a3c1009edd9e05
cgalvaojr <cirogalvao@biud.com.br>
2024-01-14 19:06:36 -0300

    feat: Adicionando suporte ao bootstrap

---
+++ resources/views/layouts/livraria.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="col-12">
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+					<div class="container-fluid ">
+						<div class="collapse navbar-collapse" id="navbarColor01">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item">
+									<a class="nav-link" href="#">Autores</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Assuntos</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Livros</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				<div class="container mt-2">
+					<div class="col-12">
+						@yield('content')
+					</div>
+				</div>
+
+		</div>
+	</body>
+</html>
